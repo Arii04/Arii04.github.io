d17cce49f7435f1456f0f2bad6824bb418b13cec
Arii <Kughi1590@gmail.com>
2025-04-15 19:04:11 +0100

    Trying somthn

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('mobile-menu');
-    const navLinks = document.querySelector('nav ul');
-  
-    menuToggle.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
-    });
-  });
-  
